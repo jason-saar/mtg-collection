4282e11e1f9111e6421bb3692b5e3e366c859aed
Jason Saar <jason.saar@gmail.com>
2026-07-02 19:16:25 -0700

    fix: use card face image as fallback for double faced card print previews

--- app/components/CardTable.tsx
+++ app/components/CardTable.tsx
@@ -3,18 +3,21 @@
 import Link from "next/link"
 
 type PrintInfo = {
-id: string
-set_name: string
-prices: {
-  usd?: string | null
-  usd_foil?: string | null
-  usd_etched?: string | null
-}
-image_uris?: { normal: string } | null
+  id: string
+  set_name: string
+  prices: {
+    usd?: string | null
+    usd_foil?: string | null
+    usd_etched?: string | null
+  }
+  image_uris?: { normal: string } | null
+  card_faces?: {
+    image_uris?: { normal : string} | null
+  }[]
 }
 
 interface CardTableProps {
-cards: PrintInfo[]
+  cards: PrintInfo[]
 }
 
 export default function CardTable({ cards }: CardTableProps) {
@@ -27,34 +30,36 @@ export default function CardTable({ cards }: CardTableProps) {
         </tr>
       </thead>
       <tbody>
-        {cards.map(({ id, set_name, prices, image_uris }) => ( 
-          <tr key={id} className="group relative">
-            <td className="px-2 py-1 relative">   
-              <Link href={`/cards/${id}`}>
-                {set_name}
-              </Link>         
-              {image_uris && (
-                <img 
-                  src={image_uris.normal}
-                  alt={set_name}
-                  className="hidden group-hover:block absolute right-full bottom-0"
-                  style={{ borderRadius: "4.75% / 3.5%" }}
-                />
-              )}
-            </td>
-            <td className="px-2 py-1 text-right">
-              {/* fall back through price tiers: regular->foil->etched->none */}
-              {prices.usd 
-                ? `$${prices.usd}`
-                : prices.usd_foil
-                ? `F $${prices.usd_foil}`
-                : prices.usd_etched
-                ? `E $${prices.usd_etched}`
-                : '-'
-              }
-            </td>
-          </tr>  
-        ))}
+        {cards.map(({ id, set_name, prices, image_uris, card_faces }) => {
+          const imagePreview = image_uris?.normal ?? card_faces?.[0].image_uris?.normal
+          return (
+            <tr key={id} className="group relative">
+              <td className="px-2 py-1 relative">   
+                <Link href={`/cards/${id}`}>{set_name}</Link>         
+                {imagePreview && (
+                  <img 
+                    src={imagePreview}
+                    alt={set_name}
+                    className="hidden group-hover:block absolute right-full bottom-0"
+                    style={{ borderRadius: "4.75% / 3.5%" }}
+                  />
+                )}
+
+              </td>
+              <td className="px-2 py-1 text-right">
+                {/* fall back through price tiers: regular->foil->etched->none */}
+                {prices.usd 
+                  ? `$${prices.usd}`
+                  : prices.usd_foil
+                  ? `F $${prices.usd_foil}`
+                  : prices.usd_etched
+                  ? `E $${prices.usd_etched}`
+                  : '-'
+                }
+              </td>
+            </tr>
+          )
+        })}
       </tbody>
     </table>
   )
