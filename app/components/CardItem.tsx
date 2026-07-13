485e2ee8dfe9972dcee0054ec2ee3eec68d245f1
jason-saar <jason.saar@gmail.com>
2026-07-13 14:36:35 -0700

    feat: search results scale effect on hover

--- app/components/CardItem.tsx
+++ app/components/CardItem.tsx
@@ -11,7 +11,12 @@ export default function CardItem({ card }: CardItemProps){
     return (
         <Link href={`/cards/${card.id}`}>
             {card.image_uris?.normal && (
-                <img src={card.image_uris.normal} alt={card.name} style={{ borderRadius: "4.75% / 3.5%" }} />
+                <img 
+                    src={card.image_uris.normal}
+                    alt={card.name}
+                    style={{ borderRadius: "4.75% / 3.5%" }}
+                    className="transition-transform duration-150 hover:scale-105 hover:shadow-lg cursor-pointer"
+                />
             )}
         </Link>
     )
